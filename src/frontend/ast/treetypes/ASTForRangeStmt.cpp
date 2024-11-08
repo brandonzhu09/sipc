099f73544c18c60a43bd0a65a882ab3936dab257
Brandon Zhu <jnt6mq@virginia.edu>
2024-11-08 15:17:54 -0500

    Updated ASTVisualizer tests

--- src/frontend/ast/treetypes/ASTForRangeStmt.cpp
+++ src/frontend/ast/treetypes/ASTForRangeStmt.cpp
@@ -6,23 +6,31 @@ void ASTForRangeStmt::accept(ASTVisitor *visitor) {
         getElement()->accept(visitor);
         getStart()->accept(visitor);
         getEnd()->accept(visitor);
-        getIncrement()->accept(visitor);
+        if (getIncrement() != nullptr) {
+            getIncrement()->accept(visitor);
+        }
         getBody()->accept(visitor);
     }
     visitor->endVisit(this);
 }
 
 std::ostream &ASTForRangeStmt::print(std::ostream &out) const {
-    out << "for (" << *getElement() << " : " << *getStart() << " .. " << *getEnd() << " by " << *getIncrement() << ") " << *getBody();
+    out << "for (" << *getElement() << " : " << *getStart() << " .. " << *getEnd();
+    if (getIncrement() != nullptr) {
+        out << " by " << *getIncrement();
+    }
+    out << ") " << *getBody();
     return out;
-} // LCOV_EXCL_LINE
+}
 
 std::vector<std::shared_ptr<ASTNode>> ASTForRangeStmt::getChildren() {
     std::vector<std::shared_ptr<ASTNode>> children;
     children.push_back(ELEMENT);
     children.push_back(START);
     children.push_back(END);
-    children.push_back(INCREMENT);
+    if (getIncrement() != nullptr) {
+        children.push_back(INCREMENT);
+    }
     children.push_back(BODY);
     return children;
 }
--- test/unit/frontend/ASTVisualizerTest.cpp
+++ test/unit/frontend/ASTVisualizerTest.cpp
@@ -36,3 +36,36 @@ TEST_CASE("ASTVisualizer: Generate dot graph", "[ASTVisualizer]") {
   REQUIRE(expectedEdgeCount ==
           GeneralHelper::countSubstrings(graph.str(), "->"));
 }
+
+TEST_CASE("ASTVisualizer: Generate dot graph for SIP", "[ASTVisualizer]") {
+    std::stringstream stream;
+    stream << R"(
+      short() {
+        var x, y, z, a;
+        a = {f1:0, f2:13};
+        y = a.f1;
+        x = [1 of 3];
+        y = x[0];
+        y++;
+        y--;
+        for (z : x) output z;
+        for (z : 1 .. 3) output z;
+        y = (#x == 3) ? y : -y;
+        return y;
+      }
+    )";
+
+    std::shared_ptr<ASTProgram> ast = std::move(ASTHelper::build_ast(stream));
+    SyntaxTree syntaxTree(ast);
+
+    std::stringstream graph;
+    ASTVisualizer visualizer(graph);
+    visualizer.buildGraph(syntaxTree);
+
+    int expectedNodeCount = 56;
+    int expectedEdgeCount = 55;
+    REQUIRE(expectedNodeCount ==
+            GeneralHelper::countSubstrings(graph.str(), "label"));
+    REQUIRE(expectedEdgeCount ==
+            GeneralHelper::countSubstrings(graph.str(), "->"));
+}
